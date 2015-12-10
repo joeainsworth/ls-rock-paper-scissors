8b64f67d7cf8643758cf0a440a3dea5a627a02e1
Joe Ainsworth <joe.ainsworth@me.com>
2015-12-10 21:05:38 +0000

    Refactored the win method to remove conditional branching

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,43 +1,45 @@
-# Ask user what their choice is
-# Validate choice
-# Computer selects choice at random
-# Compare choices and show result
-
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = {
+  'scissors' => { beats: %w(paper lizard) },
+  'paper' => { beats: %w(rock spock) },
+  'rock' => { beats: %w(lizard scissors) },
+  'lizard' => { beats: %w(spock paper) },
+  'spock' => { beats: %w(scissors rock) }
+}
 
 def prompt(msg)
   Kernel.puts("=> #{msg}")
 end
 
+def win?(first, second)
+  VALID_CHOICES[first][:beats].include?(second)
+end
+
 def display_result(player_choice, computer_choice)
-    if player_choice == 'rock' && computer_choice == 'scissors' ||
-        player_choice == 'paper' && computer_choice == 'rock' ||
-        player_choice == 'scissors' && computer_choice == 'paper'
-        'Player won!'
-    elsif computer_choice == 'rock' && player_choice == 'scissors' ||
-          computer_choice == 'paper' && player_choice == 'rock' ||
-          computer_choice == 'scissors' && player_choice == 'paper'
-          'Computer won!'
-    else
-      "It's a tie!"
-    end
+  if win?(player_choice, computer_choice)
+    'Player won!'
+  elsif win?(computer_choice, player_choice)
+    'Computer won!'
+  else
+    "It's a tie!"
+  end
 end
 
 prompt('Welcome to Rock, Paper Scissors!')
 
 loop do
   player_choice = ''
-  prompt("Please make your selection from #{VALID_CHOICES.join(", ")}")
+
+  prompt("Please make your selection from #{VALID_CHOICES.keys.join(', ')}")
   loop do
     player_choice = Kernel.gets().chomp()
-    if VALID_CHOICES.include?(player_choice.downcase)
+    if VALID_CHOICES.keys.include?(player_choice.downcase)
       break
     else
       prompt("Please enter a valid choice.")
     end
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = VALID_CHOICES.keys.sample
 
   prompt("Player chose #{player_choice} and the computer chose #{computer_choice}")
 
