08726d10feaaafecb419733136fa2de919e81507
Joe Ainsworth <joe.ainsworth@me.com>
2015-12-10 21:17:25 +0000

    Refactored game into method

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -24,27 +24,30 @@ def display_result(player_choice, computer_choice)
   end
 end
 
-prompt('Welcome to Rock, Paper Scissors!')
-
-loop do
-  player_choice = ''
-
+def players_turn
   prompt("Please make your selection from #{VALID_CHOICES.keys.join(', ')}")
+
   loop do
     player_choice = Kernel.gets().chomp()
     if VALID_CHOICES.keys.include?(player_choice.downcase)
-      break
+      return player_choice
     else
       prompt("Please enter a valid choice.")
     end
   end
+end
 
+def play_game
+  player_choice = players_turn
   computer_choice = VALID_CHOICES.keys.sample
-
   prompt("Player chose #{player_choice} and the computer chose #{computer_choice}")
-
   prompt(display_result(player_choice, computer_choice))
+end
 
+prompt('Welcome to Rock, Paper Scissors!')
+
+loop do
+  play_game
   prompt('Would you like to play again? [y] yes [n] no')
   play_again = Kernel.gets().chomp()
   break unless play_again.downcase.start_with?('y')
